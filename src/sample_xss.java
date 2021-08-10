c2c7a74265d7b9841f186091c417b3e1e3aa7d67
James Brotsos <james.brotsos@checkmarx.com>
2021-08-10 00:08:04 +0000

    Update sample_xss.java

--- src/sample_xss.java
+++ src/sample_xss.java
@@ -13,7 +13,7 @@ public class BookDetail_jsp extends HttpJspBase {
 	private static void printSanitizedName (String str) {
 
 		String sanitizedUserData = Security.validName(str);
-		String sanitizedUserData = escapeHtml4(str);
+		//String sanitizedUserData = escapeHtml4(str);
 		out.println (sanitizedUserData);
 	}
 
